a8d8f0e28ba2827cbc2f03c5bdef2b0cbdf2c436
Joshua <joshuab2@discovery.co.za>
2019-09-03 13:53:40 +0200

    Only return UserAuthentication in me endpoint

--- src/main/java/za/co/joshuabakerg/auth/authservice/controllers/UserController.java
+++ src/main/java/za/co/joshuabakerg/auth/authservice/controllers/UserController.java
@@ -1,10 +1,8 @@
 package za.co.joshuabakerg.auth.authservice.controllers;
 
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 import java.security.Principal;
 
@@ -12,8 +10,8 @@ import java.security.Principal;
 public class UserController {
 
     @GetMapping(value = "/user/me")
-    public Principal user(Principal principal) {
-        return principal;
+    public Principal user(OAuth2Authentication principal) {
+        return principal.getUserAuthentication();
     }
 
 }
